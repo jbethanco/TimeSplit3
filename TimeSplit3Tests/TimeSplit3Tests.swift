9844a4425e1d2f8ada9a995f27cb81fb812b6e4a
John B <jbethancourt.ctr@revacomm.com>
2021-01-28 16:32:03 -1000

    Initial Commit

---
+++ TimeSplit3Tests/TimeSplit3Tests.swift
@@ -0,0 +1,33 @@
+//
+//  TimeSplit3Tests.swift
+//  TimeSplit3Tests
+//
+//  Created by John Bethancourt on 1/28/21.
+//
+
+import XCTest
+@testable import TimeSplit3
+
+class TimeSplit3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
